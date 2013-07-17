977fa109d278b6237221eab1f78cbb5b42de10a0
Stephen Visser <visser.stephen@gmail.com>
2013-07-17 08:07:04 -0600

    added dist folder for hosting

--- Gruntfile.js
+++ Gruntfile.js
@@ -268,7 +268,6 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
-            'bower_components/**/*',
             'images/{,*/}*.{gif,webp,svg}',
             'styles/fonts/*'
           ]
